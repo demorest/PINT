484eda35e2ea8aca68275b595e1bb0e3ed9a0b96
luojing1211 <jingluo@Jings-MacBook-Air-4.local>
2016-10-24 11:02:31 -0500

    add numerical calculaiton for d_phase_d_param

--- pint/models/timing_model.py
+++ pint/models/timing_model.py
@@ -320,7 +320,9 @@ class TimingModel(object):
         Either analytically, or numerically
         """
         result = 0.0
-
+        # TODO need to do correct chain rule stuff wrt delay derivs, etc
+        # Is it safe to assume that any param affecting delay only affects
+        # phase indirectly (and vice-versa)??
         an_funcname = "d_phase_d_" + param
         if hasattr(self, an_funcname):
             # Have an analytic function for this parameter
@@ -331,16 +333,29 @@ class TimingModel(object):
         return result
 
     #@Cache.use_cache
-    def d_phase_d_param_num(self, toas, param):
+    def d_phase_d_param_num(self, toas, param, step=1e-2):
         """ Return the derivative of phase with respect to the parameter.
 
         NOT implemented yet.
         """
-        result = 0.0
+        # TODO : We need to know the range of parameter. 
+        par = getattr(self, param)
+        ori_value = par.value
+        if ori_value == 0:
+            h = 1.0 * step
+        else:
+            h = ori_value * step
+        parv = [par.value-h, par.value+h]
 
-        # TODO need to do correct chain rule stuff wrt delay derivs, etc
-        # Is it safe to assume that any param affecting delay only affects
-        # phase indirectly (and vice-versa)??
+        phaseI = np.zeros((len(toas),2))
+        phaseF = np.zeros((len(toas),2))
+        for ii, val in enumerate(parv):
+            par.value = val
+            phaseI[:,ii] = self.phase(t.table).int
+            phaseF[:,ii] = self.phase(t.table).frac
+        resI = (- phaseI[:,0] + phaseI[:,0])
+        resF = (- phaseF[:,0] + phaseF[:,0])
+        result = (resI + resF)/(2.0 * h)
         return result
 
     def d_delay_d_param(self, toas, param):
